8ad5107fbacf72f4894b80edcccfb8b5cd4fa219
terrablue <102580937+terrablue@users.noreply.github.com>
2023-09-09 14:55:18 +0300

    add runtime-compat/meta

---
+++ runtime-compat/meta/exports.js
@@ -0,0 +1,5 @@
+const packager = import.meta.runtime?.packager ?? "npm";
+const manifest = import.meta.runtime?.manifest ?? "package.json";
+const library = import.meta.runtime?.library ?? "node_modules";
+
+export {packager, manifest, library};
