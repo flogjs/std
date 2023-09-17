7aec3bc49bac051d06245c1de8e5b03f3a75aa92
terrablue <102580937+terrablue@users.noreply.github.com>
2023-09-17 13:38:38 +0300

    invariant: add more tests for every, pass all arguments from exports

--- runtime-compat/invariant/exports.js
+++ runtime-compat/invariant/exports.js
@@ -2,7 +2,7 @@ import Is from "./src/Is.js";
 import Every from "./src/Every.js";
 
 const is = value => new Is(value);
-const every = value => new Every(value);
+const every = (...values) => new Every(...values);
 const defined = (value, error) => is(value).defined(error);
 
 export {is, every, defined};
