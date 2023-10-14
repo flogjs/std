883a64ee1211a0959dc59041845fd59ce14cf429
terrablue <102580937+terrablue@users.noreply.github.com>
2023-10-15 01:32:53 +0300

    add meta.depend

--- runtime-compat/meta/exports.js
+++ runtime-compat/meta/exports.js
@@ -3,4 +3,5 @@ const manifest = import.meta.runtime?.manifest ?? "package.json";
 const library = import.meta.runtime?.library ?? "node_modules";
 const runtime = typeof Bun === "undefined" ? "node" : "bun";
 
-export {packager, manifest, library, runtime};
+export { packager, manifest, library, runtime };
+import { default as depend } from "./depend.js";
